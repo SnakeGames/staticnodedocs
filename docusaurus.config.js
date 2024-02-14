b233b5904472663b85e020553b04bae32296ad20
Docs <admin@staticnode.org>
2024-02-14 09:00:17 +0000

    Fixed redir issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SnakeGames/staticnodedocs',
+            'https://github.com/SnakeGames/staticnodedocs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SnakeGames/staticnodedocs',
+            'https://github.com/SnakeGames/staticnodedocs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
