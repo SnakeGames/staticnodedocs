bfb34850c600f36ecca7fd17a7c549dac1f8379b
Zachary C. (SN) <zacharyc.sn@zaines-Mac-mini.local>
2024-02-17 04:06:29 -0500

    Adding + Making Changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,9 +76,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/SnakeGames/staticnodedocs',
             label: 'GitHub',
@@ -90,7 +90,7 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Documentation',
+            title: 'StaticNode - Quick Links',
             items: [
               {
                 label: 'Documentation',
