c0a48186bca5e3edb006968d952bb02029225fcc
Zachary C. (SN) <zacharyc.sn@zaines-Mac-mini.local>
2024-02-17 03:52:51 -0500

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Get Started!
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`StaticNode | Documentation`}
+      description="Welcome to - StaticNode Documentation!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
